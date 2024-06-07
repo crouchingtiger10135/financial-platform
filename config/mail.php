103daa28e5e9726419b737786a405252cbba1eac
crouchingtiger10135 <123414776+crouchingtiger10135@users.noreply.github.com>
2024-06-07 13:46:33 +1000

    Update mail.php

--- config/mail.php
+++ config/mail.php
@@ -1,8 +1,5 @@
 <?php
 return [
-
-    'default' => env('MAIL_MAILER', 'smtp'),
-
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
@@ -12,47 +9,10 @@ return [
             'username' => env('MAIL_USERNAME'),
             'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
-            'auth_mode' => null,
-        ],
-
-        'mailgun' => [
-            'transport' => 'mailgun',
-        ],
-
-        'ses' => [
-            'transport' => 'ses',
-        ],
-
-        'postmark' => [
-            'transport' => 'postmark',
-        ],
-
-        'sendmail' => [
-            'transport' => 'sendmail',
-            'path' => '/usr/sbin/sendmail -bs',
-        ],
-
-        'log' => [
-            'transport' => 'log',
-            'channel' => env('MAIL_LOG_CHANNEL'),
-        ],
-
-        'array' => [
-            'transport' => 'array',
         ],
     ],
-
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
-
-    'markdown' => [
-        'theme' => 'default',
-
-        'paths' => [
-            resource_path('views/vendor/mail'),
-        ],
-    ],
-
 ];
