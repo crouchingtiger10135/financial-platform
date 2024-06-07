7e2aef4cebabf6f2a9fc26a0a8a129a91a6bbfff
crouchingtiger10135 <123414776+crouchingtiger10135@users.noreply.github.com>
2024-06-07 12:50:49 +1000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Blade::component('components.navigation', 'navigation');
-        \URL::forceScheme('https');
+        if (config('app.env') !== 'local') {
+    \URL::forceScheme('https');
+}
     }
 }
