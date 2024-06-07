25785697dcb0c3e4494fc3754591a3d3014f638f
crouchingtiger10135 <123414776+crouchingtiger10135@users.noreply.github.com>
2024-06-07 12:46:26 +1000

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Blade::component('components.navigation', 'navigation');
+        \URL::forceScheme('https');
     }
 }
