74249a3b827f26eeda67628abb5505f7d01bdc09
crouchingtiger10135 <123414776+crouchingtiger10135@users.noreply.github.com>
2024-06-07 18:42:40 +1000

    Update 2024_06_03_051748_create_documents_table.php

--- database/migrations/2024_06_03_051748_create_documents_table.php
+++ database/migrations/2024_06_03_051748_create_documents_table.php
@@ -4,27 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateDocumentsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up()
-{
-    Schema::create('documents', function (Blueprint $table) {
-        $table->id();
-        $table->unsignedBigInteger('client_id');
-        $table->string('file_path');
-        $table->string('file_name');
-        $table->timestamps();
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained()->onDelete('cascade');
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->timestamps();
+        });
+    }
 
-        $table->foreign('client_id')->references('id')->on('clients')->onDelete('cascade');
-    });
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
 }
-
-public function down()
-{
-    Schema::dropIfExists('documents');
-}
-
-};
