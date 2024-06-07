7a4b5f11d9b8e2d32a6939e943624deaccb4918c
crouchingtiger10135 <123414776+crouchingtiger10135@users.noreply.github.com>
2024-06-07 10:05:59 +1000

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
+Alpine.start();
+
+import 'bootstrap';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
