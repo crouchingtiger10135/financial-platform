b4bbddd71a325470ce9db85a38366e6c0d96f1b6
crouchingtiger10135 <123414776+crouchingtiger10135@users.noreply.github.com>
2024-06-07 19:02:59 +1000

    Update trustedproxy.php

--- config/trustedproxy.php
+++ config/trustedproxy.php
@@ -1,8 +1,9 @@
 <?php
 
 // config/trustedproxy.php
+use Illuminate\Http\Request;
 
 return [
-    'proxies' => '*',
-    'headers' => \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL,
+    'proxies' => '*', // Trust all proxies
+    'headers' => Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB,
 ];
